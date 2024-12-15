661e3a541e772e5ef8e5c34e60bfdaf9f0393537
Vladislav <vsdev@Mac-mini-Vladislav.local>
2024-12-15 23:57:53 +0100

    fix: high DPI scaling issues on macOS 5K displays, remove redundant App.kt

--- composeApp/src/desktopMain/kotlin/org/example/project/Main.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/Main.kt
@@ -9,19 +9,31 @@ import androidx.compose.ui.window.rememberWindowState
 import org.example.project.ui.components.showInfo
 import org.example.project.ui.screens.HomeScreen
 import org.example.project.ui.theme.AppTheme
+import java.awt.Toolkit
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "ENDEcode v2.0.0",
-        state = rememberWindowState(size = DpSize(1024.dp, 768.dp))
-    ) {
-        LaunchedEffect(Unit) {
-            showInfo()
-        }
+fun main() {
+    System.setProperty("sun.java2d.uiScale", "1.0")
 
-        AppTheme {
-            HomeScreen(window)
+    val scaleFactor = Toolkit.getDefaultToolkit().screenResolution / 96.0
+
+    val windowWidth = (1024 / scaleFactor).dp
+    val windowHeight = (768 / scaleFactor).dp
+
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "ENDEcode v2.0.1",
+            state = rememberWindowState(size = DpSize(windowWidth, windowHeight))
+        ) {
+            window.rootPane.putClientProperty("apple.awt.contentScaleFactor", 1.0f)
+
+            LaunchedEffect(Unit) {
+                showInfo()
+            }
+
+            AppTheme {
+                HomeScreen(window)
+            }
         }
     }
 }
